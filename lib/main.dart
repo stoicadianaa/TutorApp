c33dc1641858b371dcbe3118c01be57acc64505f
Andra Stoica <andra.stoica123@gmail.com>
2022-04-22 17:45:12 +0300

    TUT-2:As a user, I want to be able to login using an e-mail and my password.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tutor_app/screens/first_screen.dart';
 import 'package:tutor_app/screens/register_screen.dart';
 import 'package:tutor_app/screens/main_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,16 +18,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const RegisterScreen(),
-      initialRoute: RegisterScreen.id,
-      routes: {
-        RegisterScreen.id : (context) => RegisterScreen(),
-        MainScreen.id : (context) => MainScreen(),
-      }
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const FirstScreen(),
+        initialRoute: FirstScreen.id,
+        routes: {
+          FirstScreen.id: (context) => FirstScreen(),
+          RegisterScreen.id: (context) => RegisterScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          MainScreen.id: (context) => MainScreen(),
+        });
   }
 }
