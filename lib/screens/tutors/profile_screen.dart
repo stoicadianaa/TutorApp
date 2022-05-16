a96582a44124bdfe1a02bcd97437b96d966e117c
Andra Stoica <andra.stoica123@gmail.com>
2022-05-16 20:29:42 +0300

    TUT-3: Added the rating widget.

--- lib/screens/tutors/profile_screen.dart
+++ lib/screens/tutors/profile_screen.dart
@@ -14,6 +14,32 @@ class _TutorProfileState extends State<TutorProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Tutor\'s Profile')),
-        body: Container());
+        body: Column(
+          children: [
+            Center(
+              child: MaterialButton(
+                padding: const EdgeInsets.symmetric(vertical: 4),
+                onPressed: () {},
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      '4.5',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                    const SizedBox(height: 2),
+                    const Text(
+                      'Rating',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
