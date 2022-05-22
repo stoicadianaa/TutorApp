4c3587c1668554089aa14f78b0b0a5b1882b3ac6
Andra Stoica <andra.stoica123@gmail.com>
2022-05-22 21:44:37 +0300

    TUT-4: Import the tutor's courses from database.

---
+++ lib/screens/tutors/get_requests.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Requests {
+  String title, dayOfTheWeek, startTime, student, currentStatus;
+  Requests(this.title, this.dayOfTheWeek, this.startTime, this.student,
+      this.currentStatus);
+}
+
+List<Requests> listOfRequests =
+    List.filled(0, Requests(' ', ' ', ' ', ' ', ' '), growable: true);
+
+void getRequests(String? tutor) async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Requests courseToAdd;
+  var _myRequestsSnapshots = await firestore
+      .collection('users-courses-requests')
+      .where("tutor-email", isEqualTo: tutor)
+      .get();
+
+  final _requests = _myRequestsSnapshots.docs;
+
+  int i;
+  for (i = 0; i < _requests.length; i++) {
+    courseToAdd = Requests(
+      _requests[i].data()['course-name'],
+      _requests[i].data()['dayOfTheWeek'],
+      _requests[i].data()['startTime'],
+      _requests[i].data()['user-email'],
+      _requests[i].data()['status-request'],
+    );
+    listOfRequests.add(courseToAdd);
+  }
+}
